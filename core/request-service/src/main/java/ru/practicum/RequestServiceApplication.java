3b0bb9fa55e8e7c0797d3c9134028d91dd2f6351
Dmitrii Suntsov <ya.dimon4ik@yandex.ru>
2026-07-15 17:16:49 +0400

    add README.md

--- core/request-service/src/main/java/ru/practicum/RequestServiceApplication.java
+++ core/request-service/src/main/java/ru/practicum/RequestServiceApplication.java
@@ -3,12 +3,10 @@ package ru.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-//EnableDiscoveryClient
 @EnableFeignClients(basePackages = {
         "ru.practicum.feign.event",
         "ru.practicum.feign.user"})
